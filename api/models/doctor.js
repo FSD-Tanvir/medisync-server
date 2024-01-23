fbda7180c36b50531d1274c13f64094205d42e66
tanvirfsd <tahasanpro@gmail.com>
2024-01-23 22:59:14 +0600

    some customiozation added

--- api/models/doctor.js
+++ api/models/doctor.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 // create schema object for doctor
 const doctorSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-        minlength: 3
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 3,
+  },
+  specialization: String,
+  image: String,
+  qualification: String,
+  experience_years: Number,
+  contact: {
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+      unique: true,
     },
-    specialization: String,
-    image: String,
-    qualification: String,
-    experience_years: Number,
-    contact: {
-        email: {
-            type: String,
-            trim: true,
-            lowercase: true,
-            required: true,
-            unique: true, 
-        },
-        phone: String,
-    },
-    university: String,
+    phone: String,
+  },
+  university: String,
 });
 
 const Doctor = mongoose.model("Doctor", doctorSchema);
