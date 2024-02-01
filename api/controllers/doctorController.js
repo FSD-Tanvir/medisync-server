1639cf61468cb39ff3318c3e4da3ef47b362f23c
tanvirfsd <tahasanpro@gmail.com>
2024-02-02 02:28:34 +0600

    create post api for save a doctor data

--- api/controllers/doctorController.js
+++ api/controllers/doctorController.js
@@ -9,6 +9,17 @@ const getAllDoctors = async(req,res)=>{
     }
 }
 
+const postDoctor = async (req, res) => {
+    const doctor = req.body
+    try {
+        const result = await Doctor.create(doctor)
+        res.status(200).json(result)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
-    getAllDoctors 
+    getAllDoctors , postDoctor
 }
