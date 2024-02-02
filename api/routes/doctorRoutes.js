a8eac996758c25ec559cffb7b7376f3e4aa4e490
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-02-02 17:42:01 +0600

    create api end point get,put,delete for meet doctors dashboard and update get all routes for career page

--- api/routes/doctorRoutes.js
+++ api/routes/doctorRoutes.js
@@ -5,7 +5,10 @@ const doctorController = require('../controllers/doctorController')
 // get all doctors item
 
 router.get('/',doctorController.getAllDoctors)
+router.get('/:id',doctorController.getSingleDoctor)
 router.post('/',doctorController.postDoctor)
+router.put('/:id',doctorController.updateDoctor)
+router.delete('/:id',doctorController.deleteDoctor)
 
 
 module.exports = router;
