3865c32347c56874553469dde01df007aa544f8f
Rasibul <rasibul179@gmail.com>
2024-02-08 00:22:46 +0600

    create a schema file for productCart

---
+++ api/models/productCart.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+// create schema object for productCart
+
+const productCartSchema = new Schema({
+    productItemId: String,
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3
+    },
+    weight: String,
+    company: String,
+    image: String,
+    price: Number,
+    quantity: Number,
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+    }
+})
+
+const ProductCarts = mongoose.model("ProductCart",productCartSchema)
+module.exports = ProductCarts
