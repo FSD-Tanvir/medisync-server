1f4cc5bbebce88e14791485156be8c2e9bb750db
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-02-07 12:24:27 +0600

    userController.js updated with added checkRole funcitn and changed code in another function there

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,6 +6,10 @@ const userController = require('../controllers/userController')
 
 router.get('/',  userController.getAllUsers)
 // router.get('/',  userController.getAllUsers)
-router.post('/', userController.postUser)
+router.post('/create-user/:email', userController.postUser)
+router.get('/check-role/:email', userController.checkRole)
+
+// delete many 
+// router.delete('/deleteAll', userController.deleteAllUsers)
 
 module.exports = router;
