780aa982ad95ae77cdaa7d5573be1bee660299d6
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-02-25 15:13:50 +0600

    I have created a function named updateUserStatistics that can when new user created, userStatistics collection will be updated together with created new document and and I have created a UserStatistic model

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,8 +1,83 @@
 const User = require("../models/User");
+const UserStatistic = require("../models/userStatistics");
+
+// function for update UserStatistics collection
+const  updateUserStatistics = async() =>{
+  const totalUsers = await User.countDocuments();
+  const userStatistic = new UserStatistic({totalUsers})
+  await userStatistic.save();
+}
 
 // get all user
 const getAllUsers = async (req, res) => {
   try {
+    let startDate, endDate;
+
+    switch (req.query.filter) {
+      case "all_day":
+        startDate = new Date(1900,0,1);
+        startDate.setHours(0, 0, 0, 0);
+        endDate = new Date();
+        break;
+      case "today":
+        startDate = new Date();
+        startDate.setHours(0, 0, 0, 0);
+        endDate = new Date();
+        endDate.setHours(23,59,59,999)
+        break;
+      case "week":
+        startDate = new Date();
+        startDate.setDate(startDate.getDate() - 7);
+        startDate.setHours(0, 0, 0, 0);
+        endDate = new Date();
+        break;
+      case "month":
+        startDate = new Date();
+        startDate.setMonth(startDate.getMonth() - 1);
+        startDate.setDate(1);
+        startDate.setHours(0, 0, 0, 0);
+        endDate = new Date();
+        break;
+      case "year":
+        startDate = new Date();
+        startDate.setFullYear(startDate.getFullYear() - 1);
+        startDate.setMonth(0);
+        startDate.setDate(1);
+        startDate.setHours(0, 0, 0, 0);
+        endDate = new Date();
+        break;
+      default:
+        startDate = new Date();
+        startDate.setHours(0, 0, 0, 0);
+        endDate = new Date();
+        endDate.setHours(23,59,59,999)
+    }
+    const query = {}
+    if (req.query.filter) {
+      const userStatistics = await UserStatistic.find({date: {$gte:startDate, $lte:endDate}}).sort({date:1})
+      // console.log(userStatistics);
+      // get initial total users 
+      const initialTotalUsers = userStatistics.length > 0 ? userStatistics[0].totalUsers : 0;
+      // get final total users 
+      const finalTotalUsers = userStatistics.length > 0 ? userStatistics[userStatistics.length - 1].totalUsers : 0;
+      // get is increased or decreased
+      const increase = finalTotalUsers - initialTotalUsers;
+      const decrease = initialTotalUsers - finalTotalUsers;
+
+      // get is percentage increased or decreased
+      const percentageIncrease = (increase === 0 && initialTotalUsers === 0) ?  (0 * 100).toFixed(2): ((increase/initialTotalUsers) * 100).toFixed(2)
+      const percentageDecrease = (decrease === 0 && initialTotalUsers === 0) ? (0 * 100).toFixed(2): ((decrease/initialTotalUsers) * 100).toFixed(2)
+
+      // console.log(users);
+      return res.status(200).json({
+        message:true,
+        increase,
+        decrease,
+        percentageIncrease,
+        percentageDecrease,
+        userStatistics
+      })
+    }
     const users = await User.find({});
     res.status(200).json({
       status: true,
@@ -19,7 +94,7 @@ const getAllUsers = async (req, res) => {
 // get single user
 const getSingleUser = async (req, res) => {
   try {
-    const user = await User.findOne({email: req.params?.email});
+    const user = await User.findOne({ email: req.params?.email });
     res.status(200).json({
       status: true,
       message: "Single user gotten successfully",
@@ -45,6 +120,9 @@ const postUser = async (req, res) => {
     }
     const newUser = new User(userData);
     const savedUser = await newUser.save();
+    // update user statistics
+    await updateUserStatistics()
+
     res.status(200).json({
       status: true,
       message: "New user created successfully",
@@ -58,24 +136,26 @@ const postUser = async (req, res) => {
   }
 };
 
-// update user 
-const updateUser = async (req,res)=>{
-  
-  try{
-    const userEmail = req.params.email
-  const acknowledgement = await User.updateOne({email:userEmail},{$set:{...req.body}})
-  res.status(201).json({
-    status:true,
-    message:"Profile updated successfully",
-    acknowledgement
-  })
-  }catch(err){
+// update user
+const updateUser = async (req, res) => {
+  try {
+    const userEmail = req.params.email;
+    const acknowledgement = await User.updateOne(
+      { email: userEmail },
+      { $set: { ...req.body } }
+    );
+    res.status(201).json({
+      status: true,
+      message: "Profile updated successfully",
+      acknowledgement,
+    });
+  } catch (err) {
     res.status(500).json({
-      status:false,
-      message: err.message
-    })
+      status: false,
+      message: err.message,
+    });
   }
-}
+};
 
 // check the of role of user - if they are admin or normal user
 // const checkRole = async (req, res) => {
@@ -120,5 +200,5 @@ module.exports = {
   getAllUsers,
   postUser,
   updateUser,
-  getSingleUser
+  getSingleUser,
 };
