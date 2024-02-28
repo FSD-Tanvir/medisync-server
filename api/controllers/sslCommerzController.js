2411b7748b37ae3c4a20484c0b33d50a185cbf8f
Rasibul <rasibul179@gmail.com>
2024-02-29 00:48:24 +0600

    branch

--- api/controllers/sslCommerzController.js
+++ api/controllers/sslCommerzController.js
@@ -1,70 +1,99 @@
 const SSLCommerzModel = require('../models/sslCommerz')
 const { ObjectId } = require('mongodb');
-// sslcommerce
-const SSLCommerzPayment = require('sslcommerz-lts')
+require("dotenv").config();
+const SSLCommerzPayment = require('sslcommerz-lts');
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-// sslCommerz store id/password
-const store_id = process.env.STORE_ID
-const store_passwd = process.env.STORE_PASS
-const is_live = false //true for live, false for sandbox
 
-// post payment data
+const store_id = "teamp65d7090b9e99f";
+const store_passwd = "teamp65d7090b9e99f@ssl";
+const is_live = false; // true for live, false for sandbox
+
 const postPaymet = async (req, res) => {
-    // console.log(req.body)
-    const order = req.body
-    const tranId = new ObjectId().toString()
-    const data = {
-        total_amount: order?.subTotal,
-        currency: order?.currency,
-        tran_id: tranId, // use unique tran_id for each api call
-        success_url: `http://localhost:5000/allOrders/payment/success/${tranId}`,
-        fail_url: 'http://localhost:3030/fail',
-        cancel_url: 'http://localhost:3030/cancel',
-        ipn_url: 'http://localhost:3030/ipn',
-        shipping_method: 'Courier',
-        product_name: 'Computer.',
-        product_category: 'Electronic',
-        product_profile: 'general',
-        cus_name: order?.user_name,
-        cus_email: order?.user_email,
-        cus_add1: order?.location,
-        cus_postcode: '1000',
-        cus_country: 'Bangladesh',
-        cus_phone: '01711111111',
-        cus_fax: '01711111111',
-        ship_name: 'Customer Name',
-        ship_add1: 'Dhaka',
-        ship_add2: 'Dhaka',
-        ship_city: 'Dhaka',
-        ship_state: 'Dhaka',
-        ship_postcode: 1000,
-        ship_country: 'Bangladesh',
-    };
-    // console.log(data)
-    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-    sslcz.init(data).then(apiResponse => {
-        // Redirect the user to payment gateway
-        let GatewayPageURL = apiResponse.GatewayPageURL
-        res.send({ url: GatewayPageURL })
-        // console.log('Redirecting to: ', GatewayPageURL)
-    });
-    // try {
-    //     const newOrder = new SSLCommerzModel(req.body)
-    //     console.log(newOrder)
-    // } catch (error) {
-    //     res.status(500).json({
-    //         status: false,
-    //         message: err.message,
-    //     });
-    // }
+    try {
+        const order = req.body;
+        const tranId = new ObjectId().toString();
 
+        const data = {
+            products: order.products || [],
+            total_amount: order?.subTotal || 0,
+            currency: order?.currency || '',
+            tran_id: tranId,
+            success_url: `http://localhost:5000/allOrders/payment/success/${tranId}`,
+            fail_url: 'http://localhost:3030/fail',
+            cancel_url: 'http://localhost:3030/cancel',
+            ipn_url: 'http://localhost:3030/ipn',
+            user_name: order?.user_name || '',
+            user_email: order?.user_email || '',
+            location: order?.location || '',
+        };
+        // console.log(data)
+        const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+        sslcz.init(data).then( async apiResponse => {
+            // Redirect the user to payment gateway
+            let GatewayPageURL =apiResponse.GatewayPageURL
+            res.send({url:GatewayPageURL})
+            console.log('Redirecting to: ', GatewayPageURL)
+
+        })
+        //    res.send({ url: GatewayPageURL });
+        // const newOrder = new SSLCommerzModel({
+        //     orderId: tranId,
+        //     products: order.products,
+        //     total_amount: order.subTotal,
+        //     currency: order.currency,
+        //     success_url: data.success_url,
+        //     fail_url: data.fail_url,
+        //     cancel_url: data.cancel_url,
+        //     ipn_url: data.ipn_url,
+        //     user_name: order.user_name,
+        //     user_email: order.user_email,
+        //     location: order.location,
+        //     paidStatus: false,
+        //     tranjectionId: tranId,
+        // });
+
+        // await newOrder.save();
+        // console.log("After saving order to the database");
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: false,
+            message: error.message,
+        });
+    }
 }
 
 const tranId = async (req, res) => {
-    console.log(req.params.tranId)
-    
+    console.log(req.body.tranId)
+    // try {
+    //     const result = await SSLCommerzModel.updateOne({ tranjectionId: req.params.tranId }, {
+    //         $set: {
+    //             paidStatus: true,
+    //         }
+    //     });
+
+    //     if (result.modifiedCount > 0) {
+    //         res.redirect(`http://localhost:5173/payment/success/${req.params.tranId}`);
+    //     } else {
+    //         res.status(404).json({
+    //             status: false,
+    //             message: 'Transaction ID not found or not modified',
+    //         });
+    //     }
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({
+    //         status: false,
+    //         message: error.message,
+    //     });
+    // }
 }
 
 module.exports = {
-    postPaymet,tranId
+    postPaymet,
+    tranId
 }
--- api/routes/sslCommerzRoute.js
+++ api/routes/sslCommerzRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const sslCommerzController= require('../controllers/sslCommerzController')
 
 // post route
-router.post('/order',sslCommerzController.postPaymet)
+router.post('/',sslCommerzController.postPaymet)
 router.post('/payment/success/:tranId',sslCommerzController.tranId)
 
 module.exports = router ;
