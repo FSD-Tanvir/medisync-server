fe6ee54645806dc75072eec1f797063013122309
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-03-06 23:12:46 +0600

    I have removed and updated all things for our medicine booking payment system work properly and order data saved successfully on mongodb database and update paidStatus filed after successful payment or delete saved order if payment canceled or failed

--- api/routes/sslCommerzRoute.js
+++ api/routes/sslCommerzRoute.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const sslCommerzController= require('../controllers/sslCommerzController')
 
 // post route
-router.post('/',sslCommerzController.postPaymet)
-router.post('/payment/success/:tranId',sslCommerzController.tranId)
+router.post('/',sslCommerzController.postPayment)
+router.post('/payment/success/:tranId',sslCommerzController.updateOrder)
+router.post('/payment/failed/:tranId',sslCommerzController.deleteOrder)
+router.post('/payment/cancel/:tranId',sslCommerzController.deleteOrder)
 
 module.exports = router ;
